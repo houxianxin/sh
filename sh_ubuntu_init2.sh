03623ee3aa0ed1a179fc5a28fca1e945ce564c67
houxianxin <927630051@qq.com>
2023-06-08 17:58:50 +0800

    Update sh_ubuntu_init2.sh

--- sh_ubuntu_init2.sh
+++ sh_ubuntu_init2.sh
@@ -46,6 +46,8 @@ rm ~/miniconda.sh && \
 # 安装nvm
 export GIT_SSL_NO_VERIFY=true && \
 wget --no-check-certificate -qO- https://ghproxy.com/https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash && \
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.myshrc && \
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.myshrc && \
 echo '
 # place this after nvm initialization!
 autoload -U add-zsh-hook
